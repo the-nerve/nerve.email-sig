791a7d91ab9ee634d069c309a215a59407bd753d
Chris Hahn <chris+digital@chrishahn.me>
2021-07-25 23:23:42 -0400

    Write signatures to files

--- .bin/generateSignatures.js
+++ .bin/generateSignatures.js
@@ -1,9 +1,18 @@
+import fs from 'fs'
+import resolve from 'resolve';
+import path from 'path';
 import juice from 'juice'
 import { strict as assert } from "assert";
 import { crush, defaults } from "html-crush";
 
 import { signatureTemplate } from '../src/template.js';
 import { signatureConfig } from '../src/config.js';
+
+const OUTPUT_DIR = path.join(process.cwd(), '/output');
+
+if (!fs.existsSync(OUTPUT_DIR)){
+    fs.mkdirSync(OUTPUT_DIR);
+}
 
 // Juice Repo + Docs: https://github.com/Automattic/juice
 const juiceOptions = {
@@ -21,17 +30,34 @@ const juiceOptions = {
 
 }
 
+const writeSignatureFile = (name, signatureHTML) => {
+    const path = `${OUTPUT_DIR}/${name}.html`
+    fs.writeFile( path, signatureHTML, err => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        console.log(`Signature generated at: ${path}`);
+      })
+}
+
 const hydratedSignatures = signatureConfig.people.map( person => {
     const hydratedTemplate = signatureTemplate({...person})
     const signature = juice(hydratedTemplate, juiceOptions);
-    return signature;
+    return {
+        name: person.firstName.toLowerCase(),
+        html: signature
+    }
 })
 
 console.log(hydratedSignatures);
 
-const minifiedSignatures = hydratedSignatures.map( signatureHTML => {
-    const signature = crush( signatureHTML, defaults ).result
-    return signature;
+const minifiedSignatures = hydratedSignatures.map( ({name, html}) => {
+    const signature = crush( html, defaults ).result
+    return {
+        name,
+        html: signature
+    };
 })
 
-console.log(minifiedSignatures)
+minifiedSignatures.forEach( ({name, html}) => writeSignatureFile(name, html))
